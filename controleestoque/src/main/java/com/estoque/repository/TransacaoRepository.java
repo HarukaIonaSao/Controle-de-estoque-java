08d6d35effdaeb9adc33165add53ac6d0f7a619d
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-12-08 10:19:19 -0300

    [feat]: Adição de busca por id do produto

--- controleestoque/src/main/java/com/estoque/repository/TransacaoRepository.java
+++ controleestoque/src/main/java/com/estoque/repository/TransacaoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.estoque.model.Transacao;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long>{
+    List<Transacao> findByProdutoId(Integer produto_id);
     List<Transacao> findByTipo(String tipo);
     List<Transacao> findByQuantidadeGreaterThan(Integer quantidade);
     List<Transacao> findByDataBetween(LocalDateTime startDate, LocalDateTime endDate); 
