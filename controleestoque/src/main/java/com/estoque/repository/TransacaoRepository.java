6e5593749786cf6c53904c56a5375f1e8e7fb434
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-12-08 10:11:26 -0300

    [feat]: Traansacao repository

--- controleestoque/src/main/java/com/estoque/repository/TransacaoRepository.java
+++ controleestoque/src/main/java/com/estoque/repository/TransacaoRepository.java
@@ -1,1 +1,15 @@
 package com.estoque.repository; 
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estoque.model.Transacao;
+
+public interface TransacaoRepository extends JpaRepository<Transacao, Long>{
+    List<Transacao> findByTipo(String tipo);
+    List<Transacao> findByQuantidadeGreaterThan(Integer quantidade);
+    List<Transacao> findByDataBetween(LocalDateTime startDate, LocalDateTime endDate); 
+
+}
